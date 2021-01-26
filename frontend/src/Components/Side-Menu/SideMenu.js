23ad37f0a6d3cbcf675d8f4dac388425fe4097f1
iCode-Cat <devranboyac8@gmail.com>
2021-01-26 18:54:46 +0200

    Header C Side bar added

--- frontend/src/Components/Side-Menu/SideMenu.js
+++ frontend/src/Components/Side-Menu/SideMenu.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './side-menu.scss'
+import SIDE_MENU from './side-menu-data'
+
+
 
 const SideMenu = () => {
 
-   return <section className="side-menu-container">
+    const [sideMenu, setSideMenuData] = useState(SIDE_MENU)
+    console.log(sideMenu);
+
+   return <section className="side-menu-container ">
        
+        <button className="compose"> <i class="fas fa-plus"></i> Compose</button>
+
+      <div className="menu-options-container border-bt">
+      {sideMenu.map(menu => {
+         return <div key={menu.id} className="menu-options hover-menu">
+         <i className={menu.icon}></i>
+         <h1>{menu.title}</h1>
+         
+         </div>
+      })}
+      </div>
        
    </section>
 
---
+++ frontend/src/Components/Side-Menu/side-menu-data.js
@@ -0,0 +1,38 @@
+const SIDE_MENU = [
+   {id:1, 
+    title:'Inbox',
+    icon:'fas fa-inbox'
+ },
+ {id:2, 
+    title:'Starred',
+    icon:'fas fa-star'
+ }
+ ,
+ {id:3, 
+    title:'Snoozed',
+    icon:'fas fa-clock'
+ }
+ ,
+ {id:4, 
+    title:'Important',
+    icon:'fas fa-arrow-circle-right'
+ }
+ ,
+ {id:5, 
+    title:'Sent',
+    icon:'fas fa-share-square'
+ }
+ ,
+ {id:6, 
+    title:'Drafts',
+    icon:'fas fa-paste'
+ }
+ ,
+ {id:7, 
+    title:'Categories',
+    icon:'fas fa-filter'
+ }
+  ];
+  
+  export default SIDE_MENU;
+  
