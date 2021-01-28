7c45bc9ed1e872a130690048a6938fb0d014d621
iCode-Cat <devranboyac8@gmail.com>
2021-01-28 11:46:30 +0200

    Message component fixed

--- frontend/src/Components/Message/Message.js
+++ frontend/src/Components/Message/Message.js
@@ -3,18 +3,27 @@ import './message.scss'
 
 const Message = ({loop}) => {
 
-    
-  
-
-    
-
     console.log(loop);
     
 
     return <div className="message-container">
         
-        {loop.map((a, index)=>{ return <h1 key={index}>HELLO</h1>})}
-
+        {loop.map((a, index)=>{ return <div className="message-card">
+            <header>
+                <p>New Message</p>
+                <div className="card-icons"></div>
+            </header>
+            <section className="message-send">
+                <input type="text" placeholder='To' className="send-to border-bt"/>
+                <input type="text" placeholder='Subject' className="send-subject border-bt"/>
+                <textarea name="" id="" cols="30" rows="10" className='send-message'></textarea>
+            </section>
+            <footer>
+                <button className="submit">Send</button>
+            </footer>
+        </div>
+})}
+        
     </div>
 
 }
