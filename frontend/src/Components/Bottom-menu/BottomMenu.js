7c71880aebcd4cada21b1697bf7263f58e3c6d30
iCode-Cat <devranboyac8@gmail.com>
2021-02-06 23:35:07 +0200

    border bottom active class added

--- frontend/src/Components/Bottom-menu/BottomMenu.js
+++ frontend/src/Components/Bottom-menu/BottomMenu.js
@@ -1,9 +1,54 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Login from '../Login/Login';
+import './bottom-menu.scss'
 const BottomMenu = () => {
 
 
-    return <h1 className='border-bt'>BOTTOM MENU</h1>
+ const [activeClass, setActiveClass] = useState({
+
+     links:[{
+         id:1,
+         i:'fa-inbox',
+         name:'Primary'
+     },
+     {
+        id:2,
+        i:'fa-user-friends',
+        name:'Social'
+     },
+     {
+        id:3,
+        i:'fa-tag',
+        name:'Promotions'
+     }
+    ],
+    currentId:1
+ });
+
+ 
+   const idHandler = (id) => {
+
+    setActiveClass((prevStyle) => ({
+        ...prevStyle,
+        currentId:id
+    }))
+    
+
+   }
+   console.log(activeClass.currentId);
+
+    return <div className="bottom-menu-container border-bt">
+    
+    {
+        activeClass.links.map((a)=>(
+            <div className={`bottom-option ${a.id === activeClass.currentId && 'bottom-active'+a.id }`} onClick={()=>{idHandler(a.id)}}>
+        <i className={`fas ${a.i}`}></i>
+        <h2>{a.name}</h2>
+        </div>
+        ))
+    }
+    
+    </div>
 
 }
 
